d8462a9ae1179668a8e6768b2c0c08ee3ea64db5
john-lasala <johnblasala@gmail.com>
2020-04-05 23:32:44 -0400

    homepage design changes

--- src/COMPONENTS/Body.js
+++ src/COMPONENTS/Body.js
@@ -5,30 +5,27 @@ const Body = props => {
     return (
         <html>
         <body>
+            <section>
             <h1>
                 Title Here
             </h1>
             <h3>
-                <i>Heading</i> 
+                Heading
             </h3>
-
+            </section>
             <br/>
             <section className="info">
                 <form action="mailto:johnlalalasala@gmail.com" method="post">
                     <span>
-                        <label for="body">Name:</label>
                         <input type="text" name="name" placeholder="Your name"/>
                     </span><br/>
                     <span>
-                        <label for="email">Email:</label>
                         <input required type="text" name="text" placeholder="Email"/>
                     </span><br/>
                     <span>
-                        <label for="body">Quantity:</label>
                         <input type="number" name="name" placeholder="Max: 50"/>
                     </span><br/>
                     <span>
-                        <label for="email">Date:</label>
                     <input required type="date" name="text" placeholder="(012)-345-6789"/>
                     </span><br/>
                     <button type="submit">Submit</button>
